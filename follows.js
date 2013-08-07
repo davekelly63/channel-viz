56fea1d34c916ca43d493b29b7914171b7d06527
davekelly63 <dave.a.kelly@gmail.com>
2013-08-07 05:27:59 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -17,7 +17,7 @@
 		applicationName	= 'DK Electricity', // Replaces Xively logo in the header
 		dataDuration	= '1hour', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
-		dataColor		= '0c5da5', // CSS HEX value of color to represent data (omit leading #)
+		dataColor		= 'FF7A00', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
